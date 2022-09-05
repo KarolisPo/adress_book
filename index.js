5f6c11d8d36718bd9e40c711d7e0999719f49ad0
KarolisPo <karo.pociu@gmail.com>
2022-09-05 21:54:05 +0300

    progress

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
-const addForm = document.querySelector("#addForm");
+const addForm = document.querySelector("#addContact");
 const addBtn = document.querySelector(".addBtn");
 const adressBook = document.querySelector("#adressBook");
 
 const contacts = [];
 
-addBtn.addEventListener("click", (e) => {
+addForm.addEventListener("submit", (e) => {
   e.preventDefault();
 
   const contactName = document.querySelector("#name");
@@ -15,16 +15,29 @@ addBtn.addEventListener("click", (e) => {
     person[contactName.name] = contactName.value;
     person[phoneNum.name] = phoneNum.value;
     contacts.push(person);
+    localStorage.setItem("contacts", JSON.stringify(contacts));
+
+    if (adressBook.firstChild) {
+      adressBook.innerHTML = null;
+    }
+
+    if (contacts.length > 0) {
+      contacts.forEach((singleContact) => {
+        const contactArr = Object.entries(singleContact);
+        const card = document.createElement("div");
+        const editBtn = document.createElement("button");
+        const favoriteBtn = document.createElement("button");
+
+        card.textContent = `${contactArr[0][1]} : ${contactArr[1][1]}`;
+        editBtn.textContent = "Edit";
+        favoriteBtn.textContent = "Favorite";
+
+        adressBook.appendChild(card);
+        adressBook.appendChild(editBtn);
+        adressBook.appendChild(favoriteBtn);
+      });
+    }
   }
 
   console.log(contacts);
-
-  localStorage.setItem("contacts", JSON.stringify(contacts));
-
-  if (adressBook.firstChild) {
-    adressBook.innerHTML = null;
-  }
 });
-
-if (contacts.length > 0) {
-}
